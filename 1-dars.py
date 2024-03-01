b4eece1d9aac554b5503e0920d46b1447ce82b4b
Nazar-2004 <159868096+Nazar-2004@users.noreply.github.com>
2024-03-01 08:46:02 +0500

    1-dars Yangilandi

--- 1-dars.py
+++ 1-dars.py
@@ -1,7 +1,7 @@
 n = int(input("Massiv elementlari sonini kiriting n="))
 a = list(map(int,input().split())) # n- ta butun sonni bitta satrga kiritish  
 s = 0
-Maxx = max(a) # Massivning eng katta elementi
+Maxxx = max(a) # Massivning eng katta elementi
 for i in range(len(a)):
-    print('%.2f'% (a[i]/Maxx),end=" ") # Massivning eng katta elementini uning barcha elementlariga bo'lyapmiz
+    print('%.2f'% (a[i]/Maxxx),end=" ") # Massivning eng katta elementini uning barcha elementlariga bo'lyapmiz
     
